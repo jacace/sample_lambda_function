64bbfe18c9f0bd0193ecf0d2d5ca7fd27531821f
jacace <jacace@jacaces-MacBook-Air.local>
2020-11-12 10:17:01 +0000

    switched to kafkajs lib

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ npm init
 <br>
 npm install node-rdkafka
 <br>
+Note: node-rdkafka compiles native code which is platform specific so make sure you compile in an EC2 AWS AMI and install the dev tools below.
+<br>
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+<br>
+. ~/.nvm/nvm.sh
+<br>
+sudo yum groupinstall "Development Tools"
+<br>
+Then zip and upload your function
+<br>
+zip -r sample_lambda_func_nodejs .
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //created by jacace
 
 var zlib = require('zlib');
-const Kafka = require('node-rdkafka');
+const { Kafka } = require('kafkajs');
 
-module.exports.handler  = async (input, context) => {
+exports.handler = async (input, context) => {
     
     var payload = Buffer.from(input.awslogs.data, 'base64');
 
@@ -13,23 +13,44 @@ module.exports.handler  = async (input, context) => {
             context.fail(e);
         } else {
             result = JSON.parse(result.toString('ascii'));
-
-            const producer = new Kafka.Producer({
-              'bootstrap.servers': 'pkc-ep9mm.us-east-2.aws.confluent.cloud:9092',
-              'sasl.username': '3XKZQ2VLHWID6XTN',
-              'sasl.password': 'oNJRkAMtmryJk5ZuwysqJx5FDiLnrnKjCxUL2Ueq5CPelydniKKuH9fdx2hXHIcu',
-              'security.protocol': 'SASL_SSL',
-              'sasl.mechanisms': 'PLAIN',
-              'dr_msg_cb': true
-          });  
-      
-          producer.produce('jacace-ecs-app', -1, result);
-          producer.disconnect();
-
-          console.log("Data Sent:", JSON.stringify(result, null, 2));
+            sendToKafka('jacace-ecs-app', result);
+            console.log("Data Sent:", JSON.stringify(result, null, 2));
         }
     });
     
     context.succeed();
 
+}
+
+async function sendToKafka(topicName, logLine) {
+    
+    //const kafka = new Kafka({
+    //  clientId: 'lambda-func',
+    //  brokers: ['b-2.kafka-cluster.pq034k.c9.kafka.us-east-1.amazonaws.com:9092','b-1.kafka-cluster.pq034k.c9.kafka.us-east-1.amazonaws.com:9092', 'b-3.kafka-cluster.pq034k.c9.kafka.us-east-1.amazonaws.com:9092']
+    //});
+
+    const kafka = new Kafka({
+        //clientId: 'my-app',
+        brokers: ['pkc-ep9mm.us-east-2.aws.confluent.cloud:9092'], 
+        ssl: true,
+        sasl: {
+            mechanism: 'plain', // scram-sha-256 or scram-sha-512
+            username: '',
+            password: ''
+        },
+    })        
+    const producer = kafka.producer();
+    
+    try {        
+        await producer.connect();
+        await producer.send({
+         topic: topicName,
+           messages: [
+             { value: JSON.stringify(logLine, null, 2) },
+           ],
+        });        
+    }
+    catch(error) {
+        console.log("sendToKafka Error:", JSON.stringify(error, null, 2));
+    }
 }
